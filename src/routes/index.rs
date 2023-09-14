987acfdeb73d8041920afd9f8868b29348b54127
Albin Ulemark Gromer <albingromer@tutamail.com>
2023-09-14 12:50:01 +0200

    Implements url regex

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -18,22 +18,24 @@ pub fn login() -> Redirect {
 #[get("/update")]
 pub async fn update(pool: &rocket::State<PgPool>) -> () {
     let url = "https://www.svt.se/nyheter/granskning/rss.xml";
+
+    let publication_string: String = get_regex_url().captures(url).unwrap()[1].into();
+    let publication: Publication = Publication::from_str(&publication_string).unwrap();
+
     let feed = channels::get_feed(url)
     .await
     .unwrap();
 
-    let publication_string: String = get_regex_url().captures(url).unwrap()[0].into();
-    let publication = Publication::from_str(&publication_string).unwrap();
-
     let articles = channels::create_new_articles(feed.items, publication);
     
     for a in articles {
-        let _record = sqlx::query("insert into articles (id, title, link, published, topic) values ($1, $2, $3, $4, $5)")
+        let _record = sqlx::query("insert into articles (id, title, link, published, topic, publication) values ($1, $2, $3, $4, $5, $6)")
             .bind(a.id)
             .bind(a.title)
             .bind(a.link)
             .bind(a.published)
             .bind(a.topic)
+            .bind(a.publication)
             .execute(pool.inner())
             .await;
     }
